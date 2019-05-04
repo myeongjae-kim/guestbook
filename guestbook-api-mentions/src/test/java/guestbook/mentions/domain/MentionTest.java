906bc39df4d25f6b7d02e22123382f2acc5cc41c
Myeongjae Kim 김명재 <dev@myeongjae.kim>
2019-05-04 17:50:34 +0900

    [#1] Add mentionRepository

--- guestbook-api-mentions/src/test/java/guestbook/mentions/domain/MentionTest.java
+++ guestbook-api-mentions/src/test/java/guestbook/mentions/domain/MentionTest.java
@@ -7,8 +7,10 @@ import org.junit.Test;
 public class MentionTest {
     @Test
     public void builder_ValidInput_ValidOutput() {
-        Mention mention = Mention.builder().name("name").content("content").build();
+        Mention.MentionBuilder mentionBuilder = Mention.builder().name("name").content("content");
+        then(mentionBuilder).hasToString("Mention.MentionBuilder(name=name, content=content)");
 
+        Mention mention = mentionBuilder.build();
         then(mention)
                 .hasFieldOrPropertyWithValue("name", "name")
                 .hasFieldOrPropertyWithValue("content", "content");
