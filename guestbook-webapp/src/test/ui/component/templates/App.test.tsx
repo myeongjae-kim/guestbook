181473ff213c621fe906c0c5e9268299ff43cbf8
Myeongjae Kim <dev@myeongjae.kim>
2019-05-11 18:34:46 +0900

    [#9] Set environments (#16)
    * [#9] Move api to the sub-directory
    
    * [#9] Add react project
    
    - [x] Add react project with TypeScript.
    - [x] Setup JSS.
    
    * [#9] Set directory structure
    
    - [x] Add tslint
    - [x] Divide codes into 'main' and 'test'
    - [x] Replace css files with JSS
    
    - TODO: Add build, run script

---
+++ guestbook-webapp/src/test/ui/component/templates/App.test.tsx
@@ -0,0 +1,9 @@
+import Home from 'main/ui/component/templates/Home';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<Home />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
