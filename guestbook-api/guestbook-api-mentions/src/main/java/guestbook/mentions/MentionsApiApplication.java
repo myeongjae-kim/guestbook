884643d27672693d2113771641d7a59ea9b11389
Myeongjae Kim 김명재 <dev@myeongjae.kim>
2019-05-17 00:09:43 +0900

    [#19] Add sample data

--- guestbook-api/guestbook-api-mentions/src/main/java/guestbook/mentions/MentionsApiApplication.java
+++ guestbook-api/guestbook-api-mentions/src/main/java/guestbook/mentions/MentionsApiApplication.java
@@ -2,7 +2,6 @@ package guestbook.mentions;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,9 +10,5 @@ public class MentionsApiApplication {
     public static void main(String[] args) {
         SpringApplication.run(MentionsApiApplication.class, args);
     }
-
-    @GetMapping
-    public String HelloWorld() {
-        return "Hello World";
-    }
 }
+
