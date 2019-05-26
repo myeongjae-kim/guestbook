637e3c2a70144bec3298b6415b65e28b48098f07
Myeongjae Kim 김명재 <dev@myeongjae.kim>
2019-05-27 02:15:56 +0900

    [#48] Add a command line runner
    - TODO: Add name field to comment.

--- guestbook-api/guestbook-api-comments/src/main/java/guestbook/comments/CommentsApiApplication.java
+++ guestbook-api/guestbook-api-comments/src/main/java/guestbook/comments/CommentsApiApplication.java
@@ -1,7 +1,17 @@
 package guestbook.comments;
 
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.model.AttributeValue;
+import com.amazonaws.services.dynamodbv2.model.PutItemRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,6 +19,32 @@ import org.springframework.web.bind.annotation.RestController;
 public class CommentsApiApplication {
     public static void main(String[] args) {
         SpringApplication.run(CommentsApiApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner runner(AmazonDynamoDB amazonDynamoDb) {
+        return (args) -> {
+            amazonDynamoDb.putItem(getCommentRequest(
+                    4,
+                    "빌헬름 베어', '지오반니님이 그린 달 지도 Full HD로 리마스터했습니다^^",
+                    "1836-03-07T02:21:30.536Z"
+            ));
+        };
+    }
+
+    private PutItemRequest getCommentRequest(Integer mentionId, String content, String createdAt) {
+        String id = UUID.randomUUID().toString();
+
+        Map<String, AttributeValue> item = new HashMap<>();
+        item.put("id", (new AttributeValue()).withS(id));
+        item.put("mentionId", (new AttributeValue()).withN(mentionId.toString()));
+        item.put("content", (new AttributeValue()).withS(content));
+        item.put("deleted", (new AttributeValue()).withBOOL(false));
+        item.put("createdAt", (new AttributeValue()).withS(createdAt));
+
+        return (new PutItemRequest())
+                .withTableName("Comment")
+                .withItem(item);
     }
 }
 
