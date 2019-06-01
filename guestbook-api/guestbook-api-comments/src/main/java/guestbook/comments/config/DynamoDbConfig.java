c7391fe08b2fe5008b428324f0fc3fecd0c3eccd
Myeongjae Kim 김명재 <dev@myeongjae.kim>
2019-06-01 18:31:54 +0900

    [#develop] Remove unused imports

--- guestbook-api/guestbook-api-comments/src/main/java/guestbook/comments/config/DynamoDbConfig.java
+++ guestbook-api/guestbook-api-comments/src/main/java/guestbook/comments/config/DynamoDbConfig.java
@@ -1,13 +1,10 @@
 package guestbook.comments.config;
 
-import com.amazonaws.auth.AWSCredentials;
-import com.amazonaws.auth.AWSCredentialsProvider;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.client.builder.AwsClientBuilder.EndpointConfiguration;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
-import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapperConfig;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTypeConverter;
 
 import java.time.LocalDateTime;
@@ -15,12 +12,10 @@ import java.time.ZoneOffset;
 import java.util.Date;
 import java.util.TimeZone;
 
-import org.socialsignin.spring.data.dynamodb.repository.config.DynamoDBMapperFactory;
 import org.socialsignin.spring.data.dynamodb.repository.config.EnableDynamoDBRepositories;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 
 @Configuration
 @EnableDynamoDBRepositories(basePackages = "guestbook.comments.domain")
