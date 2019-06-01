1faa8674fc2821234d6e6dacc3a119024ef39e82
Myeongjae Kim 김명재 <dev@myeongjae.kim>
2019-06-01 21:08:46 +0900

    [#72] Remove unused import

--- guestbook-api/guestbook-api-comments/src/test/java/guestbook/comments/SpringDataDynamoDbTestToLearn.java
+++ guestbook-api/guestbook-api-comments/src/test/java/guestbook/comments/SpringDataDynamoDbTestToLearn.java
@@ -20,7 +20,6 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootTest(classes = {DynamoDbConfig.class})
 class SpringDataDynamoDbTestToLearn {
