9e84ad169776a12408f75f98eb7135dd04ea99f6
Myeongjae Kim 김명재 <dev@myeongjae.kim>
2019-06-03 01:39:34 +0900

    [#72] Update AwsDynamoDbMapperTestToLearn

--- guestbook-api/guestbook-api-comments/src/test/java/guestbook/comments/AwsDynamoDbMapperTestToLearn.java
+++ guestbook-api/guestbook-api-comments/src/test/java/guestbook/comments/AwsDynamoDbMapperTestToLearn.java
@@ -6,7 +6,7 @@ import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSCredentialsProvider;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.client.builder.AwsClientBuilder.EndpointConfiguration;
+import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
@@ -15,8 +15,6 @@ import com.amazonaws.services.dynamodbv2.model.CreateTableRequest;
 import com.amazonaws.services.dynamodbv2.model.DeleteTableRequest;
 import com.amazonaws.services.dynamodbv2.model.Projection;
 import com.amazonaws.services.dynamodbv2.model.ProvisionedThroughput;
-import com.amazonaws.services.dynamodbv2.model.ScanRequest;
-import com.amazonaws.services.dynamodbv2.model.ScanResult;
 import com.amazonaws.services.dynamodbv2.util.TableUtils;
 
 import guestbook.comments.domain.Comment;
@@ -33,8 +31,7 @@ class AwsDynamoDbMapperTestToLearn {
     void setup() {
         AWSCredentials awsCredentials = new BasicAWSCredentials("key1", "key2");
         AWSCredentialsProvider awsCredentialsProvider = new AWSStaticCredentialsProvider(awsCredentials);
-        EndpointConfiguration endpointConfiguration =
-                new EndpointConfiguration("http://localhost:8000", "ap-northeast-2");
+        AwsClientBuilder.EndpointConfiguration endpointConfiguration = new AwsClientBuilder.EndpointConfiguration("http://localhost:8000", "ap-northeast-2");
 
         amazonDynamoDb = AmazonDynamoDBClientBuilder.standard()
                 .withCredentials(awsCredentialsProvider)
@@ -43,6 +40,7 @@ class AwsDynamoDbMapperTestToLearn {
         dynamoDbMapper = new DynamoDBMapper(amazonDynamoDb, DynamoDBMapperConfig.DEFAULT);
 
         comment = Comment.builder()
+                .name("name")
                 .mentionId(1)
                 .content("content").build();
     }
@@ -63,28 +61,57 @@ class AwsDynamoDbMapperTestToLearn {
 
     @Test
     @Disabled
-    void putItem_ShouldBeCalledAfterTableCreation_IncreasedScannedCounts() {
-        ScanRequest scanRequest = new ScanRequest().withTableName("Comment");
-        int beforeCount = amazonDynamoDb.scan(scanRequest).getCount();
-
+    void saveAndFindItem_ShouldBeCalledAfterTableCreation_FoundItem() {
+        then(comment.getId()).isNull();
         dynamoDbMapper.save(comment);
-        System.out.println(comment.getId());
-
-        ScanResult result = amazonDynamoDb.scan(scanRequest);
-        then(result.getCount()).isEqualTo(beforeCount + 1);
+        then(comment.getId()).isNotEmpty();
     }
 
     @Test
     @Disabled
-    void putAnddeleteItem_ShouldBeCalledAfterTableCreation_SameScannedCounts() {
-        ScanRequest scanRequest = new ScanRequest().withTableName("Comment");
-        int beforeCount = amazonDynamoDb.scan(scanRequest).getCount();
-
+    void saveAndLoadItem_ShouldBeCalledAfterTableCreation_FoundItem() {
+        then(comment.getId()).isNull();
         dynamoDbMapper.save(comment);
+        then(comment.getId()).isNotEmpty();
+
+        Comment foundComment = dynamoDbMapper.load(Comment.class, comment.getId());
+
+        then(foundComment)
+                .hasFieldOrPropertyWithValue("id", comment.getId());
+    }
+
+    @Test
+    @Disabled
+    void saveAndUpdateItem_ShouldBeCalledAfterTableCreation_UpdatedItem() {
+        // given
+        then(comment.getId()).isNull();
+        dynamoDbMapper.save(comment);
+        final String commentId = comment.getId();
+
+        then(commentId).isNotEmpty();
+        then(comment).hasFieldOrPropertyWithValue("content", "content");
+
+        // when
+        comment.update("modified content");
+        dynamoDbMapper.save(comment);
+        Comment foundComment = dynamoDbMapper.load(Comment.class, commentId);
+
+        // then
+        then(foundComment)
+                .hasFieldOrPropertyWithValue("content", "modified content");
+    }
+
+    @Test
+    @Disabled
+    void saveAndDeleteItem_ShouldBeCalledAfterTableCreation_SameScannedCounts() {
+        then(comment.getId()).isNull();
+        dynamoDbMapper.save(comment);
+        final String commentId = comment.getId();
+        then(comment.getId()).isNotEmpty();
         dynamoDbMapper.delete(comment);
 
-        ScanResult result = amazonDynamoDb.scan(scanRequest);
-        then(result.getCount()).isEqualTo(beforeCount);
+        Comment comment = dynamoDbMapper.load(Comment.class, commentId);
+        then(comment).isNull();
     }
 
     @Test
@@ -94,3 +121,4 @@ class AwsDynamoDbMapperTestToLearn {
         then(TableUtils.deleteTableIfExists(amazonDynamoDb, deleteTableRequest)).isTrue();
     }
 }
+
