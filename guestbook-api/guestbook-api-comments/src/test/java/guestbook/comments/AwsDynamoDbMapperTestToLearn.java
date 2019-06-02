c1ef73f74e0123c19a94708412233e0ba26cba36
Myeongjae Kim <dev@myeongjae.kim>
2019-06-02 16:59:53 +0900

    [#72] Add a test to delete table (#73)
    * Update spring-data-dynamodb to 5.1.0
    
    * [#69] Remove redundant configs (#70)
    
    * [#develop] Remove unused imports
    
    * [#develop] Fix typo
    
    * [#72] spring-data-dynamodb test to delete a table
    
    * [#72] Remove unused import
    
    * [#72] Rename the name of test class
    
    - SpringDataDynamodbTestToLearn -> AwsDynamoDbMapperTestToLearn
    
    * [#72] Run CommandLineRunner only if a profile is set to 'local'

--- guestbook-api/guestbook-api-comments/src/test/java/guestbook/comments/AwsDynamoDbMapperTestToLearn.java
+++ guestbook-api/guestbook-api-comments/src/test/java/guestbook/comments/AwsDynamoDbMapperTestToLearn.java
@@ -2,31 +2,46 @@ package guestbook.comments;
 
 import static org.assertj.core.api.BDDAssertions.then;
 
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSCredentialsProvider;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder.EndpointConfiguration;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapperConfig;
 import com.amazonaws.services.dynamodbv2.model.CreateTableRequest;
+import com.amazonaws.services.dynamodbv2.model.DeleteTableRequest;
 import com.amazonaws.services.dynamodbv2.model.Projection;
 import com.amazonaws.services.dynamodbv2.model.ProvisionedThroughput;
 import com.amazonaws.services.dynamodbv2.model.ScanRequest;
 import com.amazonaws.services.dynamodbv2.model.ScanResult;
 import com.amazonaws.services.dynamodbv2.util.TableUtils;
 
-import guestbook.comments.config.DynamoDbConfig;
 import guestbook.comments.domain.Comment;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(classes = {DynamoDbConfig.class})
-class SpringDataDynamoDbTestToLearn {
-    private @Autowired AmazonDynamoDB amazonDynamoDb;
-    private @Autowired DynamoDBMapper dynamoDbMapper;
+class AwsDynamoDbMapperTestToLearn {
+    private AmazonDynamoDB amazonDynamoDb;
+    private DynamoDBMapper dynamoDbMapper;
     private Comment comment;
 
     @BeforeEach
     void setup() {
+        AWSCredentials awsCredentials = new BasicAWSCredentials("key1", "key2");
+        AWSCredentialsProvider awsCredentialsProvider = new AWSStaticCredentialsProvider(awsCredentials);
+        EndpointConfiguration endpointConfiguration =
+                new EndpointConfiguration("http://localhost:8000", "ap-northeast-2");
+
+        amazonDynamoDb = AmazonDynamoDBClientBuilder.standard()
+                .withCredentials(awsCredentialsProvider)
+                .withEndpointConfiguration(endpointConfiguration).build();
+
+        dynamoDbMapper = new DynamoDBMapper(amazonDynamoDb, DynamoDBMapperConfig.DEFAULT);
+
         comment = Comment.builder()
                 .mentionId(1)
                 .content("content").build();
@@ -43,7 +58,6 @@ class SpringDataDynamoDbTestToLearn {
                         .withProvisionedThroughput(new ProvisionedThroughput(1L, 1L))
                         .withProjection(new Projection().withProjectionType("ALL"))
         );
-
         then(TableUtils.createTableIfNotExists(amazonDynamoDb, createTableRequest)).isTrue();
     }
 
@@ -71,5 +85,12 @@ class SpringDataDynamoDbTestToLearn {
 
         ScanResult result = amazonDynamoDb.scan(scanRequest);
         then(result.getCount()).isEqualTo(beforeCount);
+    }
+
+    @Test
+    @Disabled
+    void deleteTable_ShouldBeCalledAfterTableCreation_TableHasBeenCreated() {
+        DeleteTableRequest deleteTableRequest = dynamoDbMapper.generateDeleteTableRequest(Comment.class);
+        then(TableUtils.deleteTableIfExists(amazonDynamoDb, deleteTableRequest)).isTrue();
     }
 }
